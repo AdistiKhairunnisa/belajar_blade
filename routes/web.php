b916868b7573d7fd731eebe8cca89e2478d3a82c
adisti khairunnisa <adistikhairunnisaa@gmail.com>
2022-08-29 12:56:36 +0700

    new com

--- routes/web.php
+++ routes/web.php
@@ -27,12 +27,14 @@ Route::get('/blog', function () {
     $blog_posts = [
     [
         "title" => "Judul Postku pertama",
+        "slug" => "judul-post-pertama",
         "author" => "Adis",
         "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo hic quae fugit omnis illo ex veritatis facilis repellendus eveniet optio quaerat eligendi asperiores ratione natus perspiciatis obcaecati amet autem nam ullam, cum quis. Natus, voluptatem. Accusantium nesciunt molestiae quasi nostrum. Consequuntur labore est distinctio recusandae. Itaque quaerat, repellat vitae earum blanditiis expedita praesentium saepe facilis recusandae mollitia impedit dicta cumque corporis fugiat ex alias beatae aspernatur maxime nam facere nihil reiciendis reprehenderit quo? Quasi quaerat aut debitis esse. Sit, ut!"
     ],
     [
         "title" => "Judul Postku kedua",
-        "author" => "Adis",
+        "slug" => "judul-post-kedua",
+        "author" => "Thistea",
         "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo hic quae fugit omnis illo ex veritatis facilis repellendus eveniet optio quaerat eligendi asperiores ratione natus perspiciatis obcaecati amet autem nam ullam, cum quis. Natus, voluptatem. Accusantium nesciunt molestiae quasi nostrum. Consequuntur labore est distinctio recusandae. Itaque quaerat, repellat vitae earum blanditiis expedita praesentium saepe facilis recusandae mollitia impedit dicta cumque corporis fugiat ex alias beatae aspernatur maxime nam facere nihil reiciendis reprehenderit quo? Quasi quaerat aut debitis esse. Sit, ut!"
     ],
 ];
@@ -51,4 +53,33 @@ Route::get('/about', function () {
         "email" => "adistikhairunnisa@gmail.com",
         "image" => "adisti.png"
     ]);
+});
+
+
+//halaman single post
+Route::get('posts/{slug}', function($slug){
+    $blog_posts = [
+        [
+            "title" => "Judul Postku pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Adis",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo hic quae fugit omnis illo ex veritatis facilis repellendus eveniet optio quaerat eligendi asperiores ratione natus perspiciatis obcaecati amet autem nam ullam, cum quis. Natus, voluptatem. Accusantium nesciunt molestiae quasi nostrum. Consequuntur labore est distinctio recusandae. Itaque quaerat, repellat vitae earum blanditiis expedita praesentium saepe facilis recusandae mollitia impedit dicta cumque corporis fugiat ex alias beatae aspernatur maxime nam facere nihil reiciendis reprehenderit quo? Quasi quaerat aut debitis esse. Sit, ut!"
+        ],
+        [
+            "title" => "Judul Postku kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Thistea",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo hic quae fugit omnis illo ex veritatis facilis repellendus eveniet optio quaerat eligendi asperiores ratione natus perspiciatis obcaecati amet autem nam ullam, cum quis. Natus, voluptatem. Accusantium nesciunt molestiae quasi nostrum. Consequuntur labore est distinctio recusandae. Itaque quaerat, repellat vitae earum blanditiis expedita praesentium saepe facilis recusandae mollitia impedit dicta cumque corporis fugiat ex alias beatae aspernatur maxime nam facere nihil reiciendis reprehenderit quo? Quasi quaerat aut debitis esse. Sit, ut!"
+        ],
+    ];
+    $new_post = [];
+    foreach($blog_posts as $post) {
+        if($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+return view('post', [
+    "title" => "Single Post",
+    "post" => $new_post
+]);
 });
